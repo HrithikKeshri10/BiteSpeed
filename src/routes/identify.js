bd60c26e621616a73dd3882255b9b75a3d7cd1dd
HrithikKeshri10 <hrithikkeshri10@gmail.com>
2025-02-09 21:46:24 +0530

    get request with message added for API endpoint

--- src/routes/identify.js
+++ src/routes/identify.js
@@ -4,5 +4,11 @@ import ContactController from "../controllers/contactController.js";
 const router = express.Router();
 
 router.post("/", ContactController.identify);
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message:
+      "Send Post request to API endpoint - https://bite-speed-tau.vercel.app/api/identify",
+  });
+});
 
 export default router;
